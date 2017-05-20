cd424b248ab0741c7bf29ccdce0749d8689016b9
mascot6699 <umangshucool@gmail.com>
2017-05-20 09:16:58 +0530

    LCA iterative solution

--- HackerRank/Trees/binary-search-tree-lowest-common-ancestor.py
+++ HackerRank/Trees/binary-search-tree-lowest-common-ancestor.py
@@ -5,3 +5,12 @@ def lca(root , v1 , v2):
     elif v1 > root.data and v2 > root.data:
         return lca(root.right, v1, v2)
     return root
+
+
+# iterative solution
+def lca(root , v1 , v2):
+    while(v1 < root.data and v2 < root.data):
+        root = root.left
+    while(v1 > root.data and v2 > root.data):
+        root = root.right
+    return root
