d9c3952cc69298cb4040cd0ac4dda1bd6f3cfd8f
mascot6699 <umangshucool@gmail.com>
2017-06-10 13:05:25 +0530

    Big number multiplication via arrays in python

---
+++ EPI/bignumber_multiplication.py
@@ -0,0 +1,15 @@
+def multiply(num1, num2):
+    sign = -1 if (num1[0] < 0) ^ (num2[0] < 0) else 1
+    num1[0], num2[0] = abs(num1[0]), abs(num2[0])
+
+    result = [0] * (len(num1) + len(num2))
+    for i in reversed(range(len(num1))):
+        for j in reversed(range(len(num2))):
+            result[i + j + 1] += num1[i] * num2[j]
+            result[i + j] += result[i + j + 1] // 10
+            result[i + j + 1] %= 10
+
+    # Remove the leading zeroes.
+    result = result[x for x in result if x != 0)] or [0]
+    return [sign * result[0]] + result[1:]
+
