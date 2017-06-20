7493d91ef047b2711837b62feb8e8a3c8c450b9e
mascot6699 <umangshucool@gmail.com>
2017-06-20 15:35:02 +0530

    find if sum of path to leaf is specified sum

--- EPI/sum_to_leaf.py
+++ EPI/sum_to_leaf.py
@@ -8,3 +8,15 @@ def sum_root_to_leaf(tree, partial_path_sum=0):
     # Non-leaf.
     return (sum_root_to_leaf(tree.left, partial_path_sum) +
             sum_root_to_leaf(tree.right, partial_path_sum))
+
+
+def has_path_sum(tree, remaining_weight):
+    if not tree:
+        return False
+    if not tree.left and not tree.right:  # Leaf.
+        return remaining_weight == tree.data
+    # Non-leaf.
+    return (has_path_sum(tree.left, remaining_weight - tree.data) or
+            has_path_sum(tree.right, remaining_weight - tree.data))
+
+
