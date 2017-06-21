0c2f357e67c2da3932b2c37f33343fb03cd7b558
mascot6699 <umangshucool@gmail.com>
2017-06-21 16:20:52 +0530

    more heap questions

--- EPI/heap.py
+++ EPI/heap.py
@@ -23,3 +23,33 @@ def simple_test():
     sort_approximately_sorted_array(iter(A), 3)
     assert result == [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+
+class Star:
+
+    def __init__(self, x, y, z):
+        self.x, self.y, self.z = x, y, z
+
+    @property
+    def distance(self):
+        return self.x**2 + self.y**2 + self.z**2
+
+    def __lt__(self, rhs):
+        return self.distance < rhs.distance
+
+    def __str__(self):
+        return ' '.join(map(str, (self.x, self.y, self.z)))
+
+    def __eq__(self, rhs):
+        return math.isclose(self.distance, rhs.distance)
+
+# note since we need to implement max heap we are using negative of what we are storing 
+# as heap in by default min heap
+def find_closest_k_stars(k, stars):
+    max_heap = []
+    reader = csv.reader(stars)
+    for line in reader:
+        star = Star(*map(float, line))
+        heapq.heappush(max_heap, (-star.distance, star))
+        if len(max_heap) == k + 1:
+            heapq.heappop(max_heap)
+    return [s[1] for s in heapq.nlargest(k, max_heap)]
