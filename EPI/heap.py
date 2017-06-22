3c9483c5d58fceefac48f12e0ab0a5d814e3585c
mascot6699 <umangshucool@gmail.com>
2017-06-22 09:54:42 +0530

    stream median using heap

--- EPI/heap.py
+++ EPI/heap.py
@@ -53,3 +53,23 @@ def find_closest_k_stars(k, stars):
         if len(max_heap) == k + 1:
             heapq.heappop(max_heap)
     return [s[1] for s in heapq.nlargest(k, max_heap)]
+
+
+
+global_result = []
+def online_median(sequence):
+    # min_heap stores the larger half seen so far.
+    min_heap = []
+    # max_heap stores the smaller half seen so far values in max_heap are negative
+    max_heap = []
+
+    for x in sequence:
+        heapq.heappush(max_heap, -heapq.heappushpop(min_heap, x))
+        # Ensure min_heap and max_heap have equal number of elements if an even
+        # number of elements is read; otherwise, min_heap must have one more
+        # element than max_heap.
+        if len(max_heap) > len(min_heap):
+            heapq.heappush(min_heap, -heapq.heappop(max_heap))
+
+        print(0.5 * (min_heap[0] + (-max_heap[0]))
+              if len(min_heap) == len(max_heap) else min_heap[0])
