9206eb7cea443a8db6f61882433f49b13c32fc16
mascot6699 <umangshucool@gmail.com>
2019-10-13 02:48:29 +0000

    Adds component

--- JS World/React/JSXReactApp/App.js
+++ JS World/React/JSXReactApp/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+
 class App extends Component{
     render(){
         return(
             <div>
-            <h1>Hello World</h1>
-        </div>
+                <Header/>
+                <Content/>
+            </div>
     );
     }
 }
+
+class Header extends React.Component {
+    render() {
+        var myStyle = {
+            fontSize: 40,
+            color: '#ff891c'
+        }
+        return (
+            <h1 style = {myStyle}>Hello World</h1>
+        );
+    }
+}
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Content</h2>
+                <p>{1+1} => I can do use javascript expressions</p>
+            </div>
+        );
+    }
+}
+
 export default App;
