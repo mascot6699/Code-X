8fb08399cf243e2b135ae2aa840bf84e2eff5046
mascot6699 <umangshucool@gmail.com>
2020-04-03 15:15:47 +0000

    Warm Observable

---
+++ JS World/React/RxJs/NOTES.md
@@ -0,0 +1,7 @@
+Notes
+--------
+
+
+- You can add another observer as a child subscription by `ob1.add(ob2)` Now if you cancel one other too will get cancelled.
+- Hot/Cold/Warm Observables
+
--- JS World/React/RxJs/src/code.ts
+++ JS World/React/RxJs/src/code.ts
@@ -1,3 +1,34 @@
-import * as Rx from "rxjs/Observable";
+import { Observable } from "rxjs/Observable";
 
-console.log(Rx);
+
+let observable = new Observable((observer:any) => {
+    try {
+        observer.next('Hey guys!')
+        observer.next('How are you?')
+        setInterval(() => {
+            observer.next('I am good')
+        }, 2000)
+    } catch (err) {
+        observer.error(err)
+    }
+})
+
+
+function addItem(val:any) {
+    let node = document.createElement("li");
+    let textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
+
+let observer1 = observable.subscribe(
+    (x:any) => addItem(x),
+    (error:any) => addItem(error),
+    () => addItem('Completed')
+);
+
+setTimeout(() => {
+    var subscription2 = observable.subscribe(
+        (x:any) => addItem('Subscriber 2: '+x)
+    );
+}, 1000);
