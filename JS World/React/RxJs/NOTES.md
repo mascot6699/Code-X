38800aaae65b3a1a13bfa1219e7bc554a5666ab2
mascot6699 <umangshucool@gmail.com>
2020-04-03 15:28:38 +0000

    Understand Subjects

--- JS World/React/RxJs/NOTES.md
+++ JS World/React/RxJs/NOTES.md
@@ -2,6 +2,10 @@ Notes
 --------
 
 
+- `subscription.unsubscribe();` to cancel a subscription
 - You can add another observer as a child subscription by `ob1.add(ob2)` Now if you cancel one other too will get cancelled.
 - Hot/Cold/Warm Observables
+- A Subject, in contrast to an observable, is simply an observer that's also able to emit values. It's both an observable and an observer simultaneously
+- events are received after it is created.
+- Behaviour subject -> BehaviorSubject is a special type of Subject whose only different is that it will emit the last value upon a new observer's subscription.
 
--- JS World/React/RxJs/src/code.ts
+++ JS World/React/RxJs/src/code.ts
@@ -1,34 +1,32 @@
-import { Observable } from "rxjs/Observable";
+import { Subject } from "rxjs/Subject";
+
+var subject = new Subject()
+
+subject.subscribe(
+    data => addItem('Observer 1: '+ data),
+    err => addItem(err),
+    () => addItem('Observer 1 Completed')
+)
+
+subject.next('The first thing has been sent')
+
+var observer2 = subject.subscribe(
+    data => addItem('Observer 2: '+ data)
+)
+
+subject.next('The second thing has been sent')
+subject.next('A third thing has been sent')
+
+observer2.unsubscribe();
+
+subject.next('A final thing has been sent')
 
 
-let observable = new Observable((observer:any) => {
-    try {
-        observer.next('Hey guys!')
-        observer.next('How are you?')
-        setInterval(() => {
-            observer.next('I am good')
-        }, 2000)
-    } catch (err) {
-        observer.error(err)
-    }
-})
-
-
+// Our handy function for showing the values:
+// We will use this shortly...
 function addItem(val:any) {
-    let node = document.createElement("li");
-    let textnode = document.createTextNode(val);
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
     node.appendChild(textnode);
     document.getElementById("output").appendChild(node);
 }
-
-let observer1 = observable.subscribe(
-    (x:any) => addItem(x),
-    (error:any) => addItem(error),
-    () => addItem('Completed')
-);
-
-setTimeout(() => {
-    var subscription2 = observable.subscribe(
-        (x:any) => addItem('Subscriber 2: '+x)
-    );
-}, 1000);
