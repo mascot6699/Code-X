5760ac359fc2c71b07ad059994bbe909858771e0
mascot6699 <umangshucool@gmail.com>
2020-04-04 20:40:21 +0000

    ReplaySubject

--- JS World/React/RxJs/NOTES.md
+++ JS World/React/RxJs/NOTES.md
@@ -8,4 +8,6 @@ Notes
 - A Subject, in contrast to an observable, is simply an observer that's also able to emit values. It's both an observable and an observer simultaneously
 - events are received after it is created.
 - BehaviourSubject => BehaviorSubject is a special type of Subject whose only different is that it will emit the last value upon a new observer's subscription.
-- ReplaySubject => ReplaySubject allows you to dispatch any designated number of values.
+- ReplaySubject => ReplaySubject allows you to dispatch any designated number of values.(first argument)
+- ReplaySubject accepts an optional second argument upon creation, which is referred to as the window time, and it's defined in milliseconds. 
+- AsyncSubject => AsyncSubject only emits the very last value, and will only do so once .complete() has been called on the subject.
--- JS World/React/RxJs/src/code.ts
+++ JS World/React/RxJs/src/code.ts
@@ -1,6 +1,6 @@
-import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { ReplaySubject } from "rxjs/ReplaySubject";
 
-var subject = new BehaviorSubject('First')
+var subject = new ReplaySubject(30, 200)
 
 subject.subscribe(
     data => addItem('Observer 1: '+ data),
@@ -8,23 +8,18 @@ subject.subscribe(
     () => addItem('Observer 1 Completed')
 )
 
-subject.next('The first thing has been sent')
-subject.next('...Observer 2 is about to subscribe...')
+var i = 1;
+var int = setInterval(() => subject.next(i++), 200);
 
-var observer2 = subject.subscribe(
-    data => addItem('Observer 2: '+ data)
-)
-
-subject.next('The second thing has been sent')
-subject.next('A third thing has been sent')
-
-observer2.unsubscribe();
-
-subject.next('A final thing has been sent')
+setTimeout(() => {
+    var observer2 = subject.subscribe(
+        data => addItem('Observer 2: '+ data)
+    )
+    clearInterval(int);
+}, 500);
 
 
-// Our handy function for showing the values:
-// We will use this shortly...
+
 function addItem(val:any) {
     var node = document.createElement("li");
     var textnode = document.createTextNode(val);
