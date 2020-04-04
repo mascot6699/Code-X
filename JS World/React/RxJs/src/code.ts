87e2b3a10731a9da632fa5a284bcd42390e3abf5
mascot6699 <umangshucool@gmail.com>
2020-04-04 20:43:15 +0000

    AsyncSubject

--- JS World/React/RxJs/src/code.ts
+++ JS World/React/RxJs/src/code.ts
@@ -1,23 +1,21 @@
-import { ReplaySubject } from "rxjs/ReplaySubject";
+import { AsyncSubject } from "rxjs/AsyncSubject";
 
-var subject = new ReplaySubject(30, 200)
+var subject = new AsyncSubject()
 
 subject.subscribe(
     data => addItem('Observer 1: '+ data),
-    err => addItem(err),
     () => addItem('Observer 1 Completed')
 )
 
 var i = 1;
-var int = setInterval(() => subject.next(i++), 200);
+setInterval(() => subject.next(i++), 100);
 
 setTimeout(() => {
     var observer2 = subject.subscribe(
         data => addItem('Observer 2: '+ data)
     )
-    clearInterval(int);
+    subject.complete();
 }, 500);
-
 
 
 function addItem(val:any) {
